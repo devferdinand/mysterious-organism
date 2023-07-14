bda0180309b0b1b4ef6d2cd361596755e12a03ac
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-07-14 14:30:26 -0400

    Fifth commit - created 30 instances of pAequor that can survive

--- main.js
+++ main.js
@@ -50,13 +50,37 @@ const pAequorFactory = (num, arr) => {
     }
 }
 
-pAequor = pAequorFactory(1, mockUpStrand());
-console.log(pAequor);
-pAequor.mutate();
-console.log(pAequor); // one of the 16 base should be different than previously
+pAequor1 = pAequorFactory(1, mockUpStrand());
+console.log(`printing specimen #${pAequor1.specimenNum}`);
+console.log(pAequor1);
+console.log(`testing mutate function on specimen #${pAequor1.specimenNum}`);
+pAequor1.mutate();
+console.log(pAequor1); // one of the 16 base should be different than previously
 
 pAequor2 = pAequorFactory(2, mockUpStrand());
+console.log(`printing specimen #${pAequor2.specimenNum}`);
 console.log(pAequor2);
-pAequor.compareDNA(pAequor2);
+console.log(`testing compareDNA function on specimen #${pAequor1.specimenNum} and specimen #${pAequor2.specimenNum}`);
+pAequor1.compareDNA(pAequor2);
+console.log(`testing willLikelySurvive function on specimen #${pAequor2.specimenNum}`);
+console.log(pAequor2.willLikelySurvive()); // returns true if pAequor2 contains 60% 'C' or 'G' bases
 
-console.log(pAequor2.willLikelySurvive());
+// create 30 instances of pAequor that can survive
+const pAequorArr = [];
+let instancesCount = 0;
+const startingSpecimenNum = 3;
+while(instancesCount !== 30){
+    pAequorX = pAequorFactory(startingSpecimenNum + instancesCount, mockUpStrand());
+    if(pAequorX.willLikelySurvive()){
+        pAequorArr.push(pAequorX);
+        instancesCount++;
+    }
+}
+console.log('30 pAequor specimen for scientific study');
+console.log(pAequorArr);
+
+// testing all 30 pAequor can survive
+console.log('testing all 30 pAequor specimen can survive');
+for(let i = 0; i < pAequorArr.length; i++){
+    console.log(`specimen #${pAequorArr[i].specimenNum} can survive in their natural environment: ${pAequorArr[i].willLikelySurvive()}`);
+}
